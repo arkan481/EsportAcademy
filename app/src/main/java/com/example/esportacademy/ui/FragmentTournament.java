7e373433379660ecd42a5d6b808fc0dd72f1be72
arkan481 <arkanharyo@gmail.com>
2020-04-11 19:41:45 +0700

    spinner

--- app/src/main/java/com/example/esportacademy/ui/FragmentTournament.java
+++ app/src/main/java/com/example/esportacademy/ui/FragmentTournament.java
@@ -8,6 +8,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.esportacademy.R;
 import com.example.esportacademy.adapters.RVTourTour;
@@ -19,6 +21,8 @@ public class FragmentTournament extends Fragment {
 
     private RecyclerView rvtour;
     private RVTourTour rvTourTour;
+    private ArrayAdapter<String> leftArray,rightArray;
+    private Spinner rightSpinner,leftSpinner;
 
     public FragmentTournament() {
         // Required empty public constructor
@@ -31,6 +35,22 @@ public class FragmentTournament extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_tournament, container, false);
         rvtour = v.findViewById(R.id.rvtour);
+        leftArray = new ArrayAdapter<>(
+                getContext(),
+                R.layout.customspinner2,
+                getResources().getStringArray(R.array.leftspinner)
+        );
+        rightArray = new ArrayAdapter<String>(
+                getContext(),
+                R.layout.customspinner2,
+                getResources().getStringArray(R.array.rightspinner)
+        );
+        rightArray.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        leftArray.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        rightSpinner = v.findViewById(R.id.rightspinner);
+        leftSpinner = v.findViewById(R.id.leftspinner);
+        leftSpinner.setAdapter(leftArray);
+        rightSpinner.setAdapter(rightArray);
         populateTournament();
         return v;
     }
